1699da11a5e07777996b39158beddad1294f1a40
Jeeva K <jeevatatva@gmail.com>
2022-08-06 18:45:26 +0530

    mdcat: install manpage

--- Formula/mdcat.rb
+++ Formula/mdcat.rb
@@ -1,9 +1,12 @@
 class Mdcat < Formula
+  include Language::Python::Virtualenv
+
   desc "Show markdown documents on text terminals"
   homepage "https://codeberg.org/flausch/mdcat"
   url "https://codeberg.org/flausch/mdcat/archive/mdcat-0.28.0.tar.gz"
   sha256 "5d7b4f4b4c1066a679cd171e7d784b4ab8cad37c44d1e1b4250a21683abff9ca"
   license "MPL-2.0"
+  revision 1
   head "https://codeberg.org/flausch/mdcat.git", branch: "main"
 
   bottle do
@@ -22,8 +25,20 @@ class Mdcat < Formula
     depends_on "pkg-config" => :build
   end
 
+  resource "asciidoc" do
+    url "https://files.pythonhosted.org/packages/8a/57/50180e0430fdb552539da9b5f96f1da6f09c4bfa951b39a6e1b4fbe37d75/asciidoc-10.2.0.tar.gz"
+    sha256 "91ff1dd4c85af7b235d03e0860f0c4e79dd1ff580fb610668a39b5c77b4ccace"
+  end
+
   def install
     system "cargo", "install", *std_cargo_args
+    # create virtualenv for generating manpage
+    venv_root = libexec/"venv"
+    venv = virtualenv_create(venv_root, "python3")
+    venv.pip_install resource("asciidoc")
+    system "#{venv_root}/bin/a2x", "-L", "--doctype", "manpage", "--format", "manpage", "mdcat.1.adoc"
+    man1.install "mdcat.1"
+    rm_rf venv_root    
   end
 
   test do
